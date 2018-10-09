321e8afb653c45860ed09f23b40bb240a674d4cb
bjorgvin773 <43991387+bjorgvin773@users.noreply.github.com>
2018-10-09 13:14:14 +0000

    created index.html with basic html code

---
+++ Desktop/Vefskólinn/1.önn/JavaScript/First demo/main.js
@@ -0,0 +1,18 @@
+var hello = "halló heimur";
+/*
+þetta er rosalega langt comment
+*/
+var tala = "5";
+
+var bool = "true";
+
+// || eða (or)
+// && og (and)
+// ! ekki (not)
+
+var JavaScriptErSpennandi = true;
+var thettaErOmurlegurDagur = false;
+
+var test3 = JavaScriptErSpennandi && thettaErOmurlegurDagur; //false
+var test4 = JavaScriptErSpennandi || thettaErOmurlegurDagur; //true
+var test5 = !thettaErOmurlegurDagur; //true
